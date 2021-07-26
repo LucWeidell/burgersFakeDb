2546193e43bdeb7a17efe8c57274c7d0d8c81c12
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-07-26 14:22:16 -0600

    fake database made

---
+++ server/db/fakeBurgersDb.js
@@ -0,0 +1,31 @@
+export const fakeBurgersDb = {
+  burgers: [
+    {
+      combo: 1,
+      name: 'Western Burger',
+      price: 10.50,
+      options: ['small', 'medium', 'large'],
+      ingredients: ['tomatoes', 'onions', 'bacon']
+    }, {
+      combo: 2,
+      name: 'Mac n Cheese Burger',
+      price: 9.50,
+      options: ['small', 'medium', 'large'],
+      ingredients: ['Mac n Cheese']
+    }, {
+      combo: 3,
+      name: 'Double Bacon Burger',
+      price: 12.75,
+      options: ['small', 'medium', 'large'],
+      ingredients: ['cheese', 'bacon', 'lettuce']
+    }, {
+      combo: 4,
+      name: 'Rich Burger',
+      price: 30,
+      options: ['small', 'medium', 'large'],
+      ingredients: ['caviar']
+    }, {
+
+    }
+  ]
+}
